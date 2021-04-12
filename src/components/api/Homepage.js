7833028b6fd3ea7b53c2c4950ee6a5a5aad234f0
student <student@students-MacBook-Pro.local>
2021-04-12 18:48:39 +0100

    adding eidget

--- src/components/api/Homepage.js
+++ src/components/api/Homepage.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import HomepageArticle from './HomepageArticle';
 
-const Homepage = ({articles}) => {
+const Homepage = ({articles, weather}) => {
 
     const homepageArticleList = articles.map((article, index) => {   
         return (
@@ -13,17 +13,49 @@ const Homepage = ({articles}) => {
         ) 
     })
 
-    return (
+    const [feels_like, setFeelsLike] = useState('');
+    const [mainTemp, setMainTemp] = useState('');
+    const [name, setName] = useState('');
+    const [icon, setIcon] = useState('');
 
+    useEffect(() => {
+        fetch ("http://api.openweathermap.org/data/2.5/weather?q=glasgow&units=metric&appid=c70067f80966f116fde30f60fdf084ad")
+        .then((res) => res.json())
+        .then((data) =>{
+            setFeelsLike(data.main.feels_like);
+            setMainTemp(data.main.temp);
+            setName(data.name);
+            setIcon(data.weather.icon);
+        })
+    }, [])
+
+
+    return (
         <div>
-        <img id="logo" src="http://localhost:3000/images/logoblack.png" alt="logo" height="100" />
-        <ul>
-            {homepageArticleList}
-        </ul>
-        <a href="#" className="fa fa-facebook bouncy"></a>
-        <a href="#" className="fa fa-twitter bouncy2"></a>
-        <a href="https://linktr.ee/tinahouston1994" className="fa fa-linkedin bouncy3"></a>
-        <a href="https://github.com/TinaHouston/Java_News_App_Frontend" className="fa fa-github bouncy4"></a>
+            <img id="logo" src="http://localhost:3000/images/logoblack.png" alt="logo" height="100" />
+            <ul>
+                {homepageArticleList}
+            </ul>
+            <a href="#" className="fa fa-facebook bouncy"></a>
+            <a href="#" className="fa fa-twitter bouncy2"></a>
+            <a href="https://linktr.ee/tinahouston1994" className="fa fa-linkedin bouncy3"></a>
+            <a href="https://github.com/TinaHouston/Java_News_App_Frontend" className="fa fa-github bouncy4"></a>
+            <p>{feels_like} and {mainTemp}</p>
+            <div id="weather_wrapper">
+	        <div className="weatherCard">
+		        <div className="currentTemp">
+			    <span className="temp">{mainTemp}&deg;</span>
+			    <span className="location">{name}</span>
+		        </div>
+		    <div className="currentWeather">
+			    <span className="conditions">{icon}</span>
+			    <div className="info">
+				<span className="rain">1.3 MM</span>
+				<span className="wind">10 MPH</span>
+			    </div>
+		    </div>
+	    </div>
+        </div>
         </div>
     )
 }
