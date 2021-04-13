85a923a5ab1d337581a7a3cf831da99baf140483
student <student@students-MacBook-Pro.local>
2021-04-13 14:01:13 +0100

    fixing article detail page

--- src/components/articles/ArticleDetail.js
+++ src/components/articles/ArticleDetail.js
@@ -15,10 +15,7 @@ const ArticleDetail = ({article}) => {
             <h5 id="article-detail-journalist">By <a href={`/articles/journalist/name/${article.journalist.name}`}>{article.journalist.name}</a></h5>
             <p className="far fa-clock">{article.date}</p>
             <h4>{article.intro}</h4>
-            <hr className="second-hr"></hr>
-            <h2>{article.articleTitle}</h2> 
-            
-            <img src = {article.image} alt="description" height="100"/>          
+            <hr className="second-hr"></hr>                     
             <p>{article.body}</p>
             </div>
         </div>
